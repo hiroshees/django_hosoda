c95f1d9ef8681656de4fd5e77c9b239775c8c769
Hiroshi Fujiwara <fujiwara@no1s.biz>
2026-07-01 07:53:54 +0000

    fix settings

--- config/settings.py
+++ config/settings.py
@@ -232,7 +232,7 @@ if RENDER:
     ]
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-if not DEBUG:
+if DEBUG:
     MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 else:
     CLOUDINARY_STORAGE = {
